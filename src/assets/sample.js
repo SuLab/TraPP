ad75bec640b994b20d0ba91992f1eb0c6b4ee614
Thomas <s2peedcoder@gmail.com>
2017-11-14 11:26:26 +0800

    reverted sample.js and added example data json file

--- src/assets/sample.js
+++ src/assets/sample.js
@@ -1,56 +1,102 @@
 const sampleNode = [
+  [
+    'Gene1',
+    'Gene2',
+    'Gene3',
+    'Gene4',
+    'Gene5',
+    'Gene6',
+    'Gene7',
+    'Gene8',
+    'Gene9',
+    'Gene10',
+    'Gene11',
+    'Gene12',
+  ],
+  [
+    'Atom1',
+    'Atom2',
+    'Atom3',
+    'Atom4',
+    'Atom5',
+    'Atom6',
+    'Atom7',
+    'Atom8',
+    'Atom9',
+    'Atom10',
+    'Atom11',
+    'Atom12',
+    'Atom9',
+    'Atom10',
+    'Atom11',
+    'Atom12',
+  ],
+];
+const sampleNode =[
   {
-    Edges: [
+    "Edges": [
       {
-        n_pmids: 1,
-        pmids: [35784],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          35784
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 2,
-        pmids: [2804636, 7508351],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 2,
+        "pmids": [
+          2804636,
+          7508351
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 1,
-        pmids: [11882918],
-        type: 'TREATS_CDtDO',
-      },
+        "n_pmids": 1,
+        "pmids": [
+          11882918
+        ],
+        "type": "TREATS_CDtDO"
+      }
     ],
-    Nodes: [
+    "Nodes": [
       {
-        label: 'Chemicals & Drugs',
-        name: 'chlorcyclizine',
+        "label": "Chemicals & Drugs",
+        "name": "chlorcyclizine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Atropine',
+        "label": "Chemicals & Drugs",
+        "name": "Atropine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Potassium',
+        "label": "Chemicals & Drugs",
+        "name": "Potassium"
       },
       {
-        label: 'Disorders',
-        name: 'Asthma',
-      },
-    ],
+        "label": "Disorders",
+        "name": "Asthma"
+      }
+    ]
   },
   {
-    Edges: [
+    "Edges": [
       {
-        n_pmids: 1,
-        pmids: [35784],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          35784
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 2,
-        pmids: [2804636, 7508351],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 2,
+        "pmids": [
+          2804636,
+          7508351
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 8,
-        pmids: [
+        "n_pmids": 8,
+        "pmids": [
           11882918,
           8777958,
           4008616,
@@ -58,45 +104,49 @@ const sampleNode = [
           10639760,
           13208020,
           9602294,
-          2772863,
+          2772863
         ],
-        type: 'ASSOCIATED_WITH_CDawDO',
-      },
+        "type": "ASSOCIATED_WITH_CDawDO"
+      }
     ],
-    Nodes: [
+    "Nodes": [
       {
-        label: 'Chemicals & Drugs',
-        name: 'chlorcyclizine',
+        "label": "Chemicals & Drugs",
+        "name": "chlorcyclizine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Atropine',
+        "label": "Chemicals & Drugs",
+        "name": "Atropine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Potassium',
+        "label": "Chemicals & Drugs",
+        "name": "Potassium"
       },
       {
-        label: 'Disorders',
-        name: 'Asthma',
-      },
-    ],
+        "label": "Disorders",
+        "name": "Asthma"
+      }
+    ]
   },
   {
-    Edges: [
+    "Edges": [
       {
-        n_pmids: 1,
-        pmids: [35784],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          35784
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 1,
-        pmids: [2834114],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          2834114
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 2,
-        pmids: [
+        "n_pmids": 2,
+        "pmids": [
           3097863,
           23045385,
           2173457,
@@ -121,177 +171,208 @@ const sampleNode = [
           2540892,
           3922535,
           6393971,
-          1320824,
+          1320824
         ],
-        type: 'ASSOCIATED_WITH_CDawDO',
-      },
+        "type": "ASSOCIATED_WITH_CDawDO"
+      }
     ],
-    Nodes: [
+    "Nodes": [
       {
-        label: 'Chemicals & Drugs',
-        name: 'chlorcyclizine',
+        "label": "Chemicals & Drugs",
+        "name": "chlorcyclizine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Atropine',
+        "label": "Chemicals & Drugs",
+        "name": "Atropine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Leukotriene D4',
+        "label": "Chemicals & Drugs",
+        "name": "Leukotriene D4"
       },
       {
-        label: 'Disorders',
-        name: 'Asthma',
-      },
-    ],
+        "label": "Disorders",
+        "name": "Asthma"
+      }
+    ]
   },
   {
-    Edges: [
+    "Edges": [
       {
-        n_pmids: 1,
-        pmids: [35784],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          35784
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 1,
-        pmids: [2834114],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          2834114
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 4,
-        pmids: [10478608, 23825762, 3959748, 6321071],
-        type: 'AUGMENTS_CDagDO',
-      },
+        "n_pmids": 4,
+        "pmids": [
+          10478608,
+          23825762,
+          3959748,
+          6321071
+        ],
+        "type": "AUGMENTS_CDagDO"
+      }
     ],
-    Nodes: [
+    "Nodes": [
       {
-        label: 'Chemicals & Drugs',
-        name: 'chlorcyclizine',
+        "label": "Chemicals & Drugs",
+        "name": "chlorcyclizine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Atropine',
+        "label": "Chemicals & Drugs",
+        "name": "Atropine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Leukotriene D4',
+        "label": "Chemicals & Drugs",
+        "name": "Leukotriene D4"
       },
       {
-        label: 'Disorders',
-        name: 'Asthma',
-      },
-    ],
+        "label": "Disorders",
+        "name": "Asthma"
+      }
+    ]
   },
   {
-    Edges: [
+    "Edges": [
       {
-        n_pmids: 1,
-        pmids: [35784],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          35784
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 2,
-        pmids: [3196970, 751438],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 2,
+        "pmids": [
+          3196970,
+          751438
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 2,
-        pmids: [8418739, 5029836],
-        type: 'TREATS_CDtDO',
-      },
+        "n_pmids": 2,
+        "pmids": [
+          8418739,
+          5029836
+        ],
+        "type": "TREATS_CDtDO"
+      }
     ],
-    Nodes: [
+    "Nodes": [
       {
-        label: 'Chemicals & Drugs',
-        name: 'chlorcyclizine',
+        "label": "Chemicals & Drugs",
+        "name": "chlorcyclizine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Atropine',
+        "label": "Chemicals & Drugs",
+        "name": "Atropine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Meperidine',
+        "label": "Chemicals & Drugs",
+        "name": "Meperidine"
       },
       {
-        label: 'Disorders',
-        name: 'Asthma',
-      },
-    ],
+        "label": "Disorders",
+        "name": "Asthma"
+      }
+    ]
   },
   {
-    Edges: [
+    "Edges": [
       {
-        n_pmids: 1,
-        pmids: [35784],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          35784
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 1,
-        pmids: [3656193],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          3656193
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 2,
-        pmids: [8082355, 10500005],
-        type: 'TREATS_CDtDO',
-      },
+        "n_pmids": 2,
+        "pmids": [
+          8082355,
+          10500005
+        ],
+        "type": "TREATS_CDtDO"
+      }
     ],
-    Nodes: [
+    "Nodes": [
       {
-        label: 'Chemicals & Drugs',
-        name: 'chlorcyclizine',
+        "label": "Chemicals & Drugs",
+        "name": "chlorcyclizine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Atropine',
+        "label": "Chemicals & Drugs",
+        "name": "Atropine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Baclofen',
+        "label": "Chemicals & Drugs",
+        "name": "Baclofen"
       },
       {
-        label: 'Disorders',
-        name: 'Asthma',
-      },
-    ],
+        "label": "Disorders",
+        "name": "Asthma"
+      }
+    ]
   },
   {
-    Edges: [
+    "Edges": [
       {
-        n_pmids: 1,
-        pmids: [35784],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          35784
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 1,
-        pmids: [3656193],
-        type: 'ASSOCIATED_WITH_CDawCD',
+        "n_pmids": 1,
+        "pmids": [
+          3656193
+        ],
+        "type": "ASSOCIATED_WITH_CDawCD"
       },
       {
-        n_pmids: 2,
-        pmids: [10378545, 10500005],
-        type: 'ASSOCIATED_WITH_CDawDO',
-      },
+        "n_pmids": 2,
+        "pmids": [
+          10378545,
+          10500005
+        ],
+        "type": "ASSOCIATED_WITH_CDawDO"
+      }
     ],
-    Nodes: [
+    "Nodes": [
       {
-        label: 'Chemicals & Drugs',
-        name: 'chlorcyclizine',
+        "label": "Chemicals & Drugs",
+        "name": "chlorcyclizine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Atropine',
+        "label": "Chemicals & Drugs",
+        "name": "Atropine"
       },
       {
-        label: 'Chemicals & Drugs',
-        name: 'Baclofen',
+        "label": "Chemicals & Drugs",
+        "name": "Baclofen"
       },
       {
-        label: 'Disorders',
-        name: 'Asthma',
-      },
-    ],
-  },
-];
+        "label": "Disorders",
+        "name": "Asthma"
+      }
+    ]
+  }
+]
 export default sampleNode;
